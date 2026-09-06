bd9c490e763b9486b85a19fca3197f90a8263c85
Zachary Stump <zstump3@wvup.edu>
2026-09-06 15:49:35 -0400

    Implemented the frontend to have the miles to kilometers conversion page

--- src/UnitConverter/Pages/Conversions.cshtml.cs
+++ src/UnitConverter/Pages/Conversions.cshtml.cs
@@ -2,12 +2,23 @@
 
 namespace UnitConverter.Pages;
 
-
+/// <summary>
+/// Handles the mile to kilometers conversion page.
+/// </summary>
 public class ConversionsModel : PageModel
 {
+    /// <summary>
+    /// Stores the number of miles
+    /// </summary>
     public string Input { get; set; } = string.Empty;
+    /// <summary>
+    /// Stores the converted number of kilometers
+    /// </summary>
     public string Output { get; set; } = string.Empty;
 
+    /// <summary>
+    /// Sets the input and converts ot from miles to kilometers
+    /// </summary>
     public void OnGet()
     {
         Input = "3.1415";
