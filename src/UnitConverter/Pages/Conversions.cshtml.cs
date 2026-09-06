0b4cacc2b4e70e2501a204ddee3aa325503599af
Zachary Stump <zstump3@wvup.edu>
2026-09-06 12:33:46 -0400

    Made the Backend/C# code. Tests pass

---
+++ src/UnitConverter/Pages/Conversions.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UnitConverter.Pages;
+
+
+public class ConversionsModel : PageModel
+{
+    public string Input { get; set; } = string.Empty;
+    public string Output { get; set; } = string.Empty;
+
+    public void OnGet()
+    {
+        Input = "3.1415";
+
+        ViewData["ConversionType"] = "Miles to Kilometers";
+        ViewData["Title"] = "Conversions";
+
+        double inputValue = Convert.ToDouble(Input);
+        double convertedValue = new UnitOf.Length()
+            .FromMiles(inputValue)
+            .ToKilometers();
+
+        Output = convertedValue.ToString();
+
+    }
+}
