148be84a6537d46ee2fa4e57ea9762ba0486cde0
Zachary Stump <zstump3@wvup.edu>
2026-09-12 20:07:28 -0400

    Finish adding the rest of the conversions. Fixed where lesson 1 tests were failing.

--- src/UnitConverter/Pages/Conversions.cshtml.cs
+++ src/UnitConverter/Pages/Conversions.cshtml.cs
@@ -1,5 +1,4 @@
-﻿using System.Globalization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace UnitConverter.Pages;
@@ -13,50 +12,89 @@ public class ConversionsModel : PageModel
     /// Stores the number of miles
     /// </summary>
     [BindProperty(SupportsGet = true)]
-    public string Input { get; set; } = string.Empty;
+    public string Input { get; set; } = "3.1415";
+
     /// <summary>
     /// Stores the converted number of kilometers
     /// </summary>
     public string Output { get; set; } = string.Empty;
-    [BindProperty(SupportsGet = true)]
-    public string ConversionType { get; set; } = string.Empty;
 
     /// <summary>
-    /// Sets the input and converts ot from miles to kilometers
+    /// Used for getting the conversion type the end user wishes to do.
+    /// </summary>
+    [BindProperty(SupportsGet = true)]
+    public string ConversionType { get; set; } = "MilesToKilometers";
+
+    /// <summary>
+    /// Used for the conversions of different types of measurements. Takes an input and displays the output of the
+    /// Conversion.
     /// </summary>
     public void OnGet()
     {
+        ViewData["ConversionType"] =
+            ConversionType.Replace("To", " to ");
+
+        ViewData["Title"] = "Conversions";
+
         //Initalize the input and validate the input.
-        double inputValue = 0;
+        double inputValue = 3.1415;
         try
         {
             inputValue = Convert.ToDouble(Input);
         }
-        catch (Exception ex)
+        catch (FormatException)
         {
-            ViewData["Error Message"] = "Input must be a valid number" + ex.Message;
+            ViewData["ErrorMessage"] = "Input must be a valid number";
+
+            return;
+        }
+        catch (OverflowException)
+        {
+            ViewData["ErrorMessage"] = "Input must be a valid number";
+
+            return;
         }
 
         //Switch expression for choosing between the conversions
+        //TODO Maybe work to add all the conversions later
         double? convertedValue = ConversionType switch
         {
+            //TODO Make the URL names syntax friendly. Take the URL input and make all lowercase.
             "MilesToKilometers" =>
                 new UnitOf.Length().FromMiles(inputValue).ToKilometers(),
 
             "KilometersToMiles" =>
                 new UnitOf.Length().FromKilometers(inputValue).ToMiles(),
 
+            "FahrenheitToCelsius" =>
+                new UnitOf.Temperature().FromFahrenheit(inputValue).ToCelsius(),
+
+            "CelsiusToFahrenheit" =>
+                new UnitOf.Temperature().FromCelsius(inputValue).ToFahrenheit(),
+
+            "PoundsToKilograms" =>
+                new UnitOf.Mass().FromPounds(inputValue).ToKilograms(),
+
+            "KilogramsToPounds" =>
+                new UnitOf.Mass().FromKilograms(inputValue).ToPounds(),
+
+            "KilobytesToTerabytes" =>
+                new UnitOf.DataStorage().FromKilobytes(inputValue).ToTerabytes(),
+
+            "TerabytesToKilobytes" =>
+                new UnitOf.DataStorage().FromTerabytes(inputValue).ToKilobytes(),
 
             _ => null
         };
 
         if (convertedValue is null)
         {
-            ViewData["Error Message"] = "Unknown conversion type";
+            ViewData["ErrorMessage"] = "Unknown conversion type";
+
+            return;
         }
 
         //Display the OutPut of the conversion
         Output = convertedValue.ToString();
-
     }
 }
